fba551745c4c0e6e05156eab0271d9ce26af51c4
Yasin Torun <yasintorun4680@gmail.com>
2023-01-09 20:20:15 +0300

    Update db.ts

--- api/db.ts
+++ api/db.ts
@@ -1,1 +1,9 @@
 import mongoose from 'mongoose'
+
+export const connect = () => {
+    mongoose.connect(process.env.DATABASE_CONNECTION_STRING || "").then(() => {
+        console.log("Connect to database")
+    }).catch((err) => {
+        console.error("Not connected to database", err)
+    })
+}
